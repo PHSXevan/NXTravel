07d09694bf6900ba75935720f626721afc9ee084
PHSXevan <631269634@qq.com>
2019-12-31 19:02:09 +0800

    半成品

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var express = require('express')
 var path = require('path')
 var router = require("./router")
+var bodyParser = require('body-parser') 
 
 var app = express()
 
 //第一个是url标识，第二个是文件目录路径
 app.use('/public/',express.static(path.join(__dirname,'./public/')))
+app.use('/views/',express.static(path.join(__dirname,'./views/')))
 app.use('/node_modules/',express.static(path.join(__dirname,'./node_modules/')))
 
 app.engine('html',require('express-art-template'))
+
 //默认是views目录，方便更改
 app.set('views',path.join(__dirname,'./views'))
 
 //把路由挂载到 app 中 
 app.use(router)
 
-app.listen(3000,function(){
+app.listen(5000,function(){
 	console.log('running...')
 })
--- router.js
+++ router.js
@@ -1,10 +1,18 @@
 var express = require('express')
-
+var fs = require('fs')
 var router = express.Router()
 
 //默认找views文件夹下的页面，无须再写路径
 router.get('/',function(req,res){
-	res.render("index.html")
+	fs.readFile('./db.json','utf8',function(err,data){
+		if(err){
+			return res.status(500).send('Server')
+		}
+		res.render("index.html", {
+			cate:JSON.parse(data).cate
+		})
+	})
+	
 })
 
 router.get('/plan',function(req,res){
